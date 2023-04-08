bb5c0f4ad8a8ec9703e86b959a1a8503e0af606e
Nasiruddin Abubakar <nasiruddinabubakar@gmail.com>
2023-04-09 03:32:14 +0500

    third commit

--- code.js
+++ code.js
@@ -9,56 +9,97 @@ console.log(players_ui);
 let dice = 0;
 let player1_score = 0;
 let player2_score = 0;
+let curr=0;
 let check = false;
 function rolldice() {
   dice = Math.trunc(Math.random() * 6 + 1);
 }
+function player1_activate(){
+
+    document.getElementById('current--0').textContent = curr;
+     document.getElementById('current--1').textContent = 0;
+    players_ui.classList.add("player--active");
+    player2.classList.remove("player--active");
+
+}
+
+function player2_activate(){
+
+    document.getElementById('current--0').textContent = 0;
+    document.getElementById('current--1').textContent = 0;
+    players_ui.classList.remove("player--active");
+    player2.classList.add("player--active");
+
+}
+function check_status(){
+    if (check) {
+    
+        player2_activate();
+      
+      } else {
+     
+        player1_activate();
+      
+      }
+}
+function update_score(){
+
+
+if (!check){
+
+    player1_score+=curr;
+    curr = 0;
+    document.getElementById('score--0').textContent=player1_score;
+}
+else{
+
+    player2_score+=curr;
+    curr = 0;
+    document.getElementById('score--1').textContent=player2_score;
+
+};
+}
+
 document.querySelector(".btn--roll").addEventListener("click", function () {
   dice = Math.trunc(Math.random() * 6 + 1);
   console.log(dice);
   switch (dice) {
     case 1:
       document.querySelector(".dice").src = "dice-1.png";
-      player1_score = 0;
-    console.log("haha");
-      if (!check) 
-      check = true;
+      curr = 0;
+      if (!check) check = true;
       else check = false;
-      break;
+        break;
     case 2:
       document.querySelector(".dice").src = "dice-2.png";
-      if (!check) player1_score+=2;
-      else player2_score+=2;
+      curr+=2;
       break;
     case 3:
       document.querySelector(".dice").src = "dice-3.png";
-      if (!check) player1_score+=3;
-      else player2_score+=3;
+      curr+=3;
       break;
     case 4:
       document.querySelector(".dice").src = "dice-4.png";
-      if (!check) player1_score+=4;
-      else player2_score+=4;
+      curr+=4;
       break;
     case 5:
       document.querySelector(".dice").src = "dice-5.png";
-      if (!check) player1_score+=5;
-      else player2_score+=5;
+      curr+=5;
       break;
     case 6:
       document.querySelector(".dice").src = "dice-6.png";
-      if (!check) player1_score+=6;
-      else player2_score+=6;
+      curr+=6;
       break;
   }
- document.getElementById('current--0').textContent = player1_score;
-  document.getElementById('current--1').textContent = player2_score;
-  if (check) {
-    players_ui.classList.remove("player--active");
-    player2.classList.add("player--active");
-  } else {
-    players_ui.classList.add("player--active");
-    player2.classList.remove("player--active");
-  }
+   
+  check_status();
 });
-document.querySelector(".btn--hold").addEventListener("click", function () {});
+document.querySelector(".btn--hold").addEventListener("click", function () {
+
+    
+    update_score(check);
+    if (!check)check=true;
+    else check = false;
+    check_status();
+     
+});
