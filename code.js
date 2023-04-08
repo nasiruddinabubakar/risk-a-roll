0a69b7a5275853f03357342ee5ea0a58210ce998
Nasiruddin Abubakar <nasiruddinabubakar@gmail.com>
2023-04-09 04:26:16 +0500

    fifth commit

--- code.js
+++ code.js
@@ -9,62 +9,46 @@ console.log(players_ui);
 let dice = 0;
 let player1_score = 0;
 let player2_score = 0;
-let curr=0;
+let curr = 0;
 let check = false;
-function rolldice() {
-  dice = Math.trunc(Math.random() * 6 + 1);
-}
-function player1_activate(){
 
-    document.getElementById('current--0').textContent = curr;
-     document.getElementById('current--1').textContent = 0;
-    players_ui.classList.add("player--active");
-    player2.classList.remove("player--active");
-
+function player1_activate() {
+  document.getElementById("current--0").textContent = curr;
+  document.getElementById("current--1").textContent = 0;
+  players_ui.classList.add("player--active");
+  player2.classList.remove("player--active");
 }
 
-function player2_activate(){
-
-    document.getElementById('current--0').textContent = 0;
-    document.getElementById('current--1').textContent = curr;
-    players_ui.classList.remove("player--active");
-    player2.classList.add("player--active");
-
+function player2_activate() {
+  document.getElementById("current--0").textContent = 0;
+  document.getElementById("current--1").textContent = curr;
+  players_ui.classList.remove("player--active");
+  player2.classList.add("player--active");
 }
-function check_status(){
-    if (check) {
-    
-        player2_activate();
-      
-      } else {
-     
-        player1_activate();
-      
-      }
+function checkStatus() {
+  if (check) {
+    player2_activate();
+  } else {
+    player1_activate();
+  }
 }
-function update_score(){
-
-
-if (!check){
-
-    player1_score+=curr;
-    if (player1_score>=100)
-        document.getElementById('name--0').textContent = "Player 1 Wins 🎉🎉";
+function updateScore() {
+  if (!check) {
+    player1_score += curr;
+    if (player1_score >= 100)
+      document.getElementById("name--0").textContent = "Player 1 Wins 🎉🎉";
     curr = 0;
-    document.getElementById('score--0').textContent=player1_score;
-}
-else{
-
-    player2_score+=curr;
-    if (player2_score>=100)
-        document.getElementById('name--1').textContent = "Player 2 Wins 🎉🎉";
+    document.getElementById("score--0").textContent = player1_score;
+  } else {
+    player2_score += curr;
+    if (player2_score >= 100)
+      document.getElementById("name--1").textContent = "Player 2 Wins 🎉🎉";
     curr = 0;
-    document.getElementById('score--1').textContent=player2_score;
-
-};
+    document.getElementById("score--1").textContent = player2_score;
+  }
 }
 
-document.querySelector(".btn--roll").addEventListener("click", function () {
+function rollTheDice() {
   dice = Math.trunc(Math.random() * 6 + 1);
   console.log(dice);
   switch (dice) {
@@ -73,50 +57,48 @@ document.querySelector(".btn--roll").addEventListener("click", function () {
       curr = 0;
       if (!check) check = true;
       else check = false;
-        break;
+      break;
     case 2:
       document.querySelector(".dice").src = "dice-2.png";
-      curr+=2;
+      curr += 2;
       break;
     case 3:
       document.querySelector(".dice").src = "dice-3.png";
-      curr+=3;
+      curr += 3;
       break;
     case 4:
       document.querySelector(".dice").src = "dice-4.png";
-      curr+=4;
+      curr += 4;
       break;
     case 5:
       document.querySelector(".dice").src = "dice-5.png";
-      curr+=5;
+      curr += 5;
       break;
     case 6:
       document.querySelector(".dice").src = "dice-6.png";
-      curr+=6;
+      curr += 6;
       break;
   }
-   
-  check_status();
-});
-document.querySelector(".btn--hold").addEventListener("click", function () {
 
-    
-    update_score(check);
-    if (!check)check=true;
-    else check = false;
-    check_status();
-     
-});
+  checkStatus();
+}
+ function holdButton() {
+  updateScore(check);
+  if (!check) check = true;
+  else check = false;
+  checkStatus();
+};
 
-document.querySelector(".btn--new").addEventListener("click",function(){
-    document.getElementById('name--1').textContent = "Player 2";
-    document.getElementById('name--0').textContent = "Player 1";
-    player1_score = 0;
-    player2_score = 0;
-    document.getElementById('current--0').textContent = 0;
-    document.getElementById('current--1').textContent = 0;
-    document.getElementById('score--1').textContent=player2_score;
-    document.getElementById('score--0').textContent=player1_score;
-    player1_activate();
+function newGame(){
 
-})
+    curr = 0;
+        document.getElementById("name--1").textContent = "Player 2";
+        document.getElementById("name--0").textContent = "Player 1";
+        player1_score = 0;
+        player2_score = 0;
+        document.getElementById("current--0").textContent = 0;
+        document.getElementById("current--1").textContent = 0;
+        document.getElementById("score--1").textContent = player2_score;
+        document.getElementById("score--0").textContent = player1_score;
+        player1_activate();
+}
